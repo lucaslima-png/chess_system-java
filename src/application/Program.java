e26afe36708fcc38bc019214027fce30371ce961
Lucas Lima <lucaslima13861@gamil.com>
2026-09-11 15:52:26 -0300

    First class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     static void main(String[] args) {
-        
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
